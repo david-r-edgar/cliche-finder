0af3e8dfb8237fa076928d4f9aaded900126dfe6
David Edgar <david.r.edgar@gmail.com>
2017-01-03 22:54:31 +0000

    refactoring, stage 1

--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,30 @@
 
+
+//class representing the input text with the text itself separated from its HTML tags
+var SeparatedHTML = function(inputText) {
+
+
+
+
+}
+
+SeparatedHTML.prototype.getPlainText = function() {
+
+}
+
+SeparatedHTML.prototype.getTagList = function() {
+
+}
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
     var highlightMatches = function(matchArray) {
         var highlightedText = document.getElementById("inputSearchText").innerText;
@@ -20,6 +46,29 @@ $(document).ready(function() {
     }
 
 
+    //requests matches from the server for the given plain text input
+    //returns promise which resolves to array of matches from the server
+    var requestMatches = function(plainTextInput) {
+        return new Promise(function(resolve, reject) {
+
+            var xhr = new XMLHttpRequest();
+            var url = "api/search";
+            var inputSearchText = encodeURIComponent(
+            document.getElementById("inputSearchText").innerText).replace(/%20/g, "+");
+            console.log("inputSearchText:", inputSearchText);
+            var params = "input=" + inputSearchText + "&content-type=text/plain";
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhr.onreadystatechange = function() {
+                if(xhr.readyState == 4 && xhr.status == 200) {
+                    console.log(xhr.responseText);
+                    highlightMatches(JSON.parse(xhr.responseText).matches);
+                    resolve(true);
+                }
+            }
+            xhr.send(params);
+        });
+    };
 
 
     // Returns a function, that, as long as it continues to be invoked, will not
@@ -42,23 +91,14 @@ $(document).ready(function() {
     }
 
     var submitSearchRequest = debounce(function() {
-        var xhr = new XMLHttpRequest();
-        var url = "api/search";
         var inputSearchText = document.getElementById("inputSearchText").innerText;
-        var inputSearchText = encodeURIComponent(
-          document.getElementById("inputSearchText").innerText).replace(/%20/g, "+");
-        console.log("inputSearchText:", inputSearchText);
-        var params = "input=" + inputSearchText + "&content-type=text/plain";
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState == 4 && xhr.status == 200) {
-                console.log(xhr.responseText);
-                highlightMatches(JSON.parse(xhr.responseText).matches);
-            }
-        }
-        xhr.send(params);
-    }, 500);
+
+        var separatedInput = new SeparatedHTML(inputSearchText);
+
+        requestMatches(separatedInput.getPlainText()).then(function(response) {
+            console.log("Success!", response);
+        });
+    }, 1500);
 
     document.getElementById("inputSearchText").addEventListener("paste", submitSearchRequest);
     document.getElementById("inputSearchText").addEventListener("keyup", submitSearchRequest);
